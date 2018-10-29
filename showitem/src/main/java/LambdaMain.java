cb2866cb92d3c78ee462ceb540acbac4b6c790b1
Fahri <fahri.yardimci@gazi.edu.tr>
2018-10-29 15:58:42 +0300

    removed unused lines

--- showitem/src/main/java/LambdaMain.java
+++ showitem/src/main/java/LambdaMain.java
@@ -1,16 +1,11 @@
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import model.Person;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
